f6c24e562edfa88c8845dbf7d73a5edd9b025b5d
Caique F. Cavalheiro <caiquefcavalheiro@gmail.com>
2023-07-16 15:42:19 -0300

    feat: added componente checkbox

---
+++ packages/react/src/components/Checkbox/index.tsx
@@ -0,0 +1,15 @@
+import { ChceckboxIndicator, CheckboxContainer } from './styles'
+import { Check } from 'phosphor-react'
+import { ComponentProps } from 'react'
+
+export type CheckboxProps = ComponentProps<typeof CheckboxContainer>
+
+export function CheckBox(props: CheckboxProps) {
+  return (
+    <CheckboxContainer {...props}>
+      <ChceckboxIndicator asChild>
+        <Check weight="bold" />
+      </ChceckboxIndicator>
+    </CheckboxContainer>
+  )
+}
